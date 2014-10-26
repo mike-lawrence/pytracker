52ae94efd63ff8d78d62521ed5675405e55751d1
eeg_imac <ray@ta6f6.wpa.dal.ca>
2014-10-26 16:50:57 -0300

    tweak example

--- example.py
+++ example.py
@@ -1,12 +1,12 @@
 if __name__ == '__main__':
 	import fileForker
-	trackerChild = fileForker.childClass(childFile='pytracker/trackerProcess')
+	trackerChild = fileForker.childClass(childFile='pytracker/trackerChild')
 	trackerChild.initDict['camIndex'] = 0
 	trackerChild.initDict['camRes'] = [1920,1080]
 	trackerChild.initDict['previewDownsize'] = 2
 	trackerChild.initDict['previewLoc'] = [0,0]
-	trackerChild.initDict['faceDetectionScale'] = 10
-	trackerChild.initDict['eyeDetectionScale'] = 5
+	trackerChild.initDict['faceDetectionScale'] = 4
+	trackerChild.initDict['eyeDetectionScale'] = 2
 	trackerChild.initDict['timestampMethod'] = 0
 	trackerChild.initDict['viewingDistance'] = 100
 	trackerChild.initDict['stimDisplayWidth'] = 100
@@ -16,7 +16,6 @@ if __name__ == '__main__':
 	trackerChild.initDict['mirrorDownSize'] = 2
 	trackerChild.initDict['manualCalibrationOrder'] = True
 	trackerChild.initDict['calibrationDotSizeInDegrees'] = 1
-	trackerChild.initDict['saccadeAlertSizeInDegrees'] = 1
 	trackerChild.start()
 	import time
 	while trackerChild.isAlive():
