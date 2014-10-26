42842a5464cf4d9ca709d6b7415abe9ec8348aa4
Mike Lawrence <mike.lwrnc@gmail.com>
2014-10-26 15:19:49 -0300

    reorganize file structure to add example

---
+++ example.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+	import fileForker
+	trackerChild = fileForker.childClass(childFile='pytracker/trackerProcess')
+	trackerChild.initDict['camIndex'] = 0
+	trackerChild.initDict['camRes'] = [1920,1080]
+	trackerChild.initDict['previewDownsize'] = 2
+	trackerChild.initDict['previewLoc'] = [0,0]
+	trackerChild.initDict['faceDetectionScale'] = 10
+	trackerChild.initDict['eyeDetectionScale'] = 5
+	trackerChild.initDict['timestampMethod'] = 0
+	trackerChild.initDict['viewingDistance'] = 100
+	trackerChild.initDict['stimDisplayWidth'] = 100
+	trackerChild.initDict['stimDisplayRes'] = [1920,1080]
+	trackerChild.initDict['stimDisplayPosition'] = [0,0]
+	trackerChild.initDict['mirrorDisplayPosition'] = [0,0]
+	trackerChild.initDict['mirrorDownSize'] = 2
+	trackerChild.initDict['manualCalibrationOrder'] = True
+	trackerChild.initDict['calibrationDotSizeInDegrees'] = 1
+	trackerChild.initDict['saccadeAlertSizeInDegrees'] = 1
+	trackerChild.start()
+	import time
+	while trackerChild.isAlive():
+		time.sleep(1)
